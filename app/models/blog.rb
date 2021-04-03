4fc80e6960ad358fa398a5142a65d078802b0c9b
Takeshi Ota <singer.scottie@gmail.com>
2021-04-03 15:29:47 +0900

    [add]image thumbnail

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,18 @@
 class Blog < ApplicationRecord
+  has_one_attached :image # サムネイル用
   has_rich_text :content
+
+  validates :image, presence: true # dafault バリデーション
+  validates :title, presence: true # dafault バリデーション
+  validate :image_content_type, if: :was_attached? #カスタムバリデータ　自作メソッドを使用する = validate
+
+  private
+  def image_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
 end
